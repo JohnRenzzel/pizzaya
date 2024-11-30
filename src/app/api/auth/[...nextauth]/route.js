bba1c65d89ea20e91af16ef49fa40f2a52944cfc
John Renzzel <ronztajanlangit09@gmail.com>
2024-11-30 21:25:27 +0800

    seven Commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/auth";
+import mongoose from "mongoose";
 
 const handler = NextAuth(authOptions);
+mongoose.connect(process.env.MONGO_URL);
 export { handler as GET, handler as POST };
