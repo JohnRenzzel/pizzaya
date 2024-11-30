4e156786b92c64e4a4d7e8ab68551fc1753485f8
John Renzzel <ronztajanlangit09@gmail.com>
2024-11-30 15:50:34 +0800

    fixes3

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
-import { signIn, useSession } from "next-auth/react";
-import Image from "next/image";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import Spinner from "@/components/layout/Spinner";
 
 export default function LoginPage() {
